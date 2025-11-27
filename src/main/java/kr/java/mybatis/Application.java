c83d998225667ecec4d095fd1c33c34aaf1950c8
kimjava911 <aibackend4th@gmail.com>
2025-11-27 10:18:22 +0900

    step02

--- src/main/java/kr/java/mybatis/Application.java
+++ src/main/java/kr/java/mybatis/Application.java
@@ -3,7 +3,7 @@ package kr.java.mybatis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 최상위에 Application <- Component Scan을 실행하는 AppConfig
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>마이바티스 게시판 프로젝트</title>
+</head>
+<body>
+    <h1>마이바티스 게시판 프로젝트</h1>
+</body>
+</html>
